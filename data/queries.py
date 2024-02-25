96b8e5251b7bac2f345fcd403fad0ccf05af6c17
gabrielssoul@ <gabrielssoul@gmail.com>
2024-02-25 14:46:46 +0100

    show movie details

--- data/queries.py
+++ data/queries.py
@@ -1,28 +1,97 @@
 from data import data_manager
+from psycopg2 import sql
 
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
 
-def get_most_rated_shows(page_num=1):
+def get_show(show_id):
     return data_manager.execute_select("""
-        SELECT title, 
-               TO_CHAR(year, 'YYYY') AS year, 
-               runtime,
-               ROUND(rating, 2) AS rating, 
-               COALESCE(trailer, 'No URL') AS trailer,
-               STRING_AGG(genres.name, ', ') AS genres, 
-               homepage FROM shows
-        LEFT JOIN show_genres ON shows.id = show_genres.show_id
-        LEFT JOIN genres ON genres.id = show_genres.genre_id
-        GROUP BY shows.id
-        ORDER BY shows.rating DESC
-        OFFSET (%(page_num)s-1)*15 LIMIT 15;
-        """, {'page_num': page_num})
+            SELECT
+                title,
+                runtime,
+                CONCAT(ROUND(rating, 1)) AS rating,
+                STRING_AGG(genres.name, ', ') AS genres,
+                trailer,
+                overview
+            FROM shows
+            LEFT JOIN show_genres ON shows.id = show_genres.show_id
+            LEFT JOIN genres ON genres.id = show_genres.genre_id
+            WHERE shows.id = %(show_id)s
+            GROUP BY shows.id
+        """, {"show_id":show_id}, fetchall=False)
+
+def get_actors(show_id):
+    return data_manager.execute_select("""
+    SELECT actors.name AS actors FROM shows
+            LEFT JOIN show_characters ON show_characters.show_id = shows.id
+            LEFT JOIN actors ON actors.id = show_characters.actor_id
+            WHERE shows.id = %(show_id)s
+            ORDER BY actor_id
+            LIMIT 3
+    """, {"show_id": show_id})
+def get_most_rated_shows(page_num=1, sort_column='rating', sort_direction='ASC'):
+    valid_sort_columns = ['title', 'year', 'runtime', 'rating', 'genres', 'homepage']
+
+    if sort_column.lower() not in valid_sort_columns:
+        sort_column = 'rating'
+
+    if sort_direction.upper() not in ['ASC', 'DESC']:
+        sort_direction = 'DESC'
+
+    query = """
+            SELECT title, 
+                   TO_CHAR(year, 'YYYY') AS year, 
+                   runtime,
+                   ROUND(rating, 2) AS rating, 
+                   COALESCE(trailer, 'No URL') AS trailer,
+                   STRING_AGG(genres.name, ', ') AS genres, 
+                   homepage 
+            FROM shows
+            LEFT JOIN show_genres ON shows.id = show_genres.show_id
+            LEFT JOIN genres ON genres.id = show_genres.genre_id
+            GROUP BY shows.id
+            ORDER BY 
+                CASE %(sort_column)s
+                    WHEN 'title' THEN title
+                    WHEN 'year' THEN year::text
+                    WHEN 'runtime' THEN runtime::text
+                    WHEN 'rating' THEN rating::text
+
+                    WHEN 'homepage' THEN homepage
+                END """+sort_direction+"""
+            OFFSET (%(page_num)s-1)*15 LIMIT 15;
+        """
+    return data_manager.execute_select(query, {'page_num': page_num, 'sort_column': sort_column})
+
+# def get_most_rated_shows(sort='title', order='asc', page=1):
+#     if order == 'asc':
+#         danger_direction = 'ASC'
+#     else:
+#         danger_direction = 'DESC'
+#     query = '''
+#             SELECT title, TO_CHAR(year,'YYYY') AS year, runtime,
+#             COALESCE(homepage, 'no url') AS homepage,
+#             COALESCE(trailer, 'no url') AS trailer,
+#             ROUND(rating,1) AS rating,
+#             STRING_AGG(genres.name, ', ') AS genre
+#             FROM shows
+#             LEFT JOIN show_genres ON shows.id = show_genres.show_id
+#             LEFT JOIN genres ON genres.id = show_genres.genre_id
+#             GROUP BY shows.id
+#             ORDER BY
+#             CASE %(sort)s
+#                 when 'title' then title
+#                 when 'year' then shows.year
+#             END
+#             '''+danger_direction+' LIMIT 15 OFFSET ((%(page)s -1) * 15);'
+#
+#     return data_manager.execute_select(query, {'page': page, 'sort': sort, 'order': order})
 
 
-def get_pages_num():
-    return data_manager.execute_select("""
-        SELECT count(*)/15 AS num FROM shows
-        """, fetchall=False)
+
+def get_page_number():
+    return data_manager.execute_select('''
+    SELECT COUNT(*) / 15 AS num FROM shows;
+    ''', fetchall=False)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 import math
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask('codecool_series')
-
 
 @app.route('/')
 def index():
@@ -17,25 +16,38 @@ def index():
 def design():
     return render_template('design.html')
 
+@app.route('/show/<int:show_id>')
+def movie_desc(show_id):
+    show=queries.get_show(show_id)
+    actors=queries.get_actors(show_id)
+    show_actors = []
+    for actor in actors:
+        show_actors.append(actor['actors'])
+    show_actors = ", ".join(show_actors)
+    show['trailer_id'] = \
+        show['trailer'][show['trailer'].find('=') + 1:] if show['trailer'] else ''
+    return render_template('movie_desc.html', show=show, actors=show_actors)
 
-@app.route('/shows/most-rated')
-@app.route('/shows/most-rated/<int:page_num>')
-def shows_most_rated(page_num=1):
-    shows = queries.get_most_rated_shows(page_num)
-    num = queries.get_pages_num()['num']
-    from_num = page_num - 2 if (page_num > 3) else 1
-    to_num = page_num + 2 if (page_num < num - 2) else num
-    if page_num > num - 2:
-        from_num = num - 4
-    if page_num < 3:
-        to_num = 5
-    return render_template('most_rated.html',
-                           page_title="Most rated shows",
-                           shows=shows,
-                           num=num,
-                           current_page=page_num,
-                           from_num=from_num,
-                           to_num=to_num)
+
+@app.route('/shows/most-rated/')
+@app.route('/shows/most-rated/<int:page>/')
+def shows_most_rated(page=1):
+    sort = request.args.get('sort', 'rating')
+    order = request.args.get('order', ' DESC')
+
+    shows = queries.get_most_rated_shows(page, sort, order)
+
+    page_number = queries.get_page_number()['num']
+    from_num = page-2 if page > 3 else 1
+    from_num = page-3 if page == page_number-2 else page-4 if page == page_number-1 else from_num
+    to_num = page if page == page_number else page + 1 if page == page_number - 1 else page + 3
+    to_num = page + 4 if page == 2 else page + 5 if page == 1 else to_num
+
+    print(sort,order)
+
+    return render_template('most_rated.html', title='Most rated shows',
+                           shows=shows, num=page_number, current_page=page,
+                           from_num=from_num, to_num=to_num, sort=sort, order=order)
 
 
 def main():
